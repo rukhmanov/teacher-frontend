8b5dc6fbdfc3a33dd90104e827b5d6d12847010f
rukhman <aleksei.rukhmanov@gmail.com>
2025-12-17 23:14:01 +0300

    ма

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: process.env['API_URL'] || 'http://localhost:3000',
+  apiUrl: process.env['API_URL'] || 'https://rukhmanov-teacher-backend-c2c0.twc1.net',
   yandexMapsApiKey: process.env['YANDEX_MAPS_API_KEY'] || '',
   dadataApiKey: process.env['DADATA_API_KEY'] || '',
 };
